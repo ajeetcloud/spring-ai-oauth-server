4bb3571c621d20625f66cbebb72336771eca80ec
Ajeet Singh <ajeet.cloud@gmail.com>
2025-07-28 02:24:56 +0530

    Initial commit for Oauth Server

---
+++ src/main/java/org/springaioauthserver/SpringAiOauthServerApplication.java
@@ -0,0 +1,13 @@
+package org.springaioauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiOauthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiOauthServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/springaioauthserver/SpringAiOauthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springaioauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiOauthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
